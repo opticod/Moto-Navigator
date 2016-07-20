8ea60385db8a918cd2d643df6b3a42a0a0b5b04f
Anupam Das <anupam.das.bwn@gmail.com>
2016-07-21 02:39:49 +0530

    Added basic direction functionalities

--- app/src/main/java/work/technie/motonavigator/MainActivity.java
+++ app/src/main/java/work/technie/motonavigator/MainActivity.java
@@ -1,17 +1,337 @@
-/*
- * Copyright (c) 2016 Anupam Das.
- */
-
 package work.technie.motonavigator;
 
+import android.Manifest;
+import android.animation.ObjectAnimator;
+import android.animation.TypeEvaluator;
+import android.animation.ValueAnimator;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.UiThread;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.PolylineOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationListener;
+import com.mapbox.mapboxsdk.location.LocationServices;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.services.Constants;
+import com.mapbox.services.android.geocoder.ui.GeocoderAutoCompleteView;
+import com.mapbox.services.commons.ServicesException;
+import com.mapbox.services.commons.geojson.LineString;
+import com.mapbox.services.commons.models.Position;
+import com.mapbox.services.directions.v5.DirectionsCriteria;
+import com.mapbox.services.directions.v5.MapboxDirections;
+import com.mapbox.services.directions.v5.models.DirectionsResponse;
+import com.mapbox.services.directions.v5.models.DirectionsRoute;
+import com.mapbox.services.geocoding.v5.GeocodingCriteria;
+import com.mapbox.services.geocoding.v5.models.GeocodingFeature;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends Activity {
+
+    private final static String TAG = "MainActivity";
+    private static final int PERMISSIONS_LOCATION = 0;
+    FloatingActionButton floatingActionButton;
+    LocationServices locationServices;
+    private MapView mapView;
+    private MapboxMap map;
+    private DirectionsRoute currentRoute;
+    private Marker markerDestination;
+    private Marker markerOrigin;
+    private Position origin;
+    private Position destination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationServices = LocationServices.getLocationServices(MainActivity.this);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                map = mapboxMap;
+
+                Location lastOrigin = locationServices.getLastLocation();
+                origin = Position.fromCoordinates(lastOrigin.getLongitude(), lastOrigin.getLatitude());
+
+                if (markerOrigin == null) {
+                    markerOrigin = map.addMarker(new MarkerOptions()
+                            .position(new LatLng(lastOrigin.getLatitude(), lastOrigin.getLongitude())).title("Origin").setSnippet("Move marker to set the new origin."));
+                }
+
+                mapboxMap.setOnMapClickListener(new MapboxMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull LatLng point) {
+
+                        destination = Position.fromCoordinates(point.getLongitude(), point.getLatitude());
+                        if (markerDestination == null) {
+                            markerDestination = map.addMarker(new MarkerOptions()
+                                    .position(point).title("Destination").setSnippet("Move marker to set the new destination."));
+                        }
+
+                        ValueAnimator markerAnimator = ObjectAnimator.ofObject(markerDestination, "position",
+                                new LatLngEvaluator(), markerDestination.getPosition(), point);
+                        markerAnimator.setDuration(1000);
+                        markerAnimator.start();
+                    }
+                });
+            }
+        });
+
+        GeocoderAutoCompleteView autocompleteDestination = (GeocoderAutoCompleteView) findViewById(R.id.query_destination);
+        autocompleteDestination.setAccessToken(getString(R.string.PUBLIC_TOKEN));
+        autocompleteDestination.setType(GeocodingCriteria.TYPE_POI);
+        autocompleteDestination.setOnFeatureListener(new GeocoderAutoCompleteView.OnFeatureListener() {
+            @Override
+            public void OnFeatureClick(GeocodingFeature feature) {
+                Position position = feature.asPosition();
+                updateMap(position.getLatitude(), position.getLongitude());
+            }
+        });
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.location_toggle_fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null) {
+                    toggleGps(!map.isMyLocationEnabled());
+                }
+            }
+        });
+
+        FloatingActionButton walkDriveActionButton = (FloatingActionButton) findViewById(R.id.walk_toggle_fab);
+        walkDriveActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null) {
+                    try {
+                        getRoute(origin, destination, DirectionsCriteria.PROFILE_WALKING);
+                    } catch (ServicesException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        FloatingActionButton bikeDriveActionButton = (FloatingActionButton) findViewById(R.id.bike_toggle_fab);
+        bikeDriveActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null) {
+                    try {
+                        getRoute(origin, destination, DirectionsCriteria.PROFILE_CYCLING);
+                    } catch (ServicesException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        FloatingActionButton carDriveActionButton = (FloatingActionButton) findViewById(R.id.car_toggle_fab);
+        carDriveActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null) {
+                    try {
+                        getRoute(origin, destination, DirectionsCriteria.PROFILE_DRIVING);
+                    } catch (ServicesException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    private void updateMap(double latitude, double longitude) {
+
+        if (markerDestination == null) {
+            markerDestination = map.addMarker(new MarkerOptions()
+                    .position(new LatLng(latitude, longitude)).title("Destination").setSnippet("Move marker to set the new destination."));
+        }
+
+        destination = Position.fromCoordinates(longitude, latitude);
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude))
+                .zoom(15)
+                .build();
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 5000, null);
+    }
+
+    private void getRoute(Position origin, Position destination, String profile) throws ServicesException {
+
+        MapboxDirections client = new MapboxDirections.Builder()
+                .setOrigin(origin)
+                .setDestination(destination)
+                .setProfile(profile)
+                .setAccessToken(getString(R.string.PUBLIC_TOKEN))
+                .build();
+
+        client.enqueueCall(new Callback<DirectionsResponse>() {
+            @Override
+            public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                // You can get the generic HTTP info about the response
+                Log.d(TAG, "Response code: " + response.code());
+                if (response.body() == null) {
+                    Log.e(TAG, "No routes found, make sure you set the right user and access token.");
+                    return;
+                }
+
+                // Print some info about the route
+                currentRoute = response.body().getRoutes().get(0);
+                Log.d(TAG, "Distance: " + currentRoute.getDistance());
+                Toast.makeText(MainActivity.this, "Route is " + currentRoute.getDistance() + " meters long.", Toast.LENGTH_SHORT).show();
+
+                // Draw the route on the map
+                drawRoute(currentRoute);
+            }
+
+            @Override
+            public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                Log.e(TAG, "Error: " + t.getMessage());
+                Toast.makeText(MainActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void drawRoute(DirectionsRoute route) {
+        // Convert LineString coordinates into LatLng[]
+        LineString lineString = LineString.fromPolyline(route.getGeometry(), Constants.OSRM_PRECISION_V5);
+        List<Position> coordinates = lineString.getCoordinates();
+        LatLng[] points = new LatLng[coordinates.size()];
+        for (int i = 0; i < coordinates.size(); i++) {
+            points[i] = new LatLng(
+                    coordinates.get(i).getLatitude(),
+                    coordinates.get(i).getLongitude());
+        }
+
+        // Draw Points on MapView
+        map.addPolyline(new PolylineOptions()
+                .add(points)
+                .color(Color.parseColor("#009688"))
+                .width(5));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @UiThread
+    public void toggleGps(boolean enableGps) {
+        if (enableGps) {
+            // Check if user has granted location permission
+            if (!locationServices.areLocationPermissionsGranted()) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_LOCATION);
+            } else {
+                enableLocation(true);
+            }
+        } else {
+            enableLocation(false);
+        }
+    }
+
+    private void enableLocation(boolean enabled) {
+        if (enabled) {
+            locationServices.addLocationListener(new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    if (location != null) {
+
+                        markerOrigin.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+                        origin = Position.fromCoordinates(location.getLongitude(), location.getLatitude());
+                        map.setCameraPosition(new CameraPosition.Builder()
+                                .target(new LatLng(location))
+                                .zoom(16)
+                                .build());
+                    }
+                }
+            });
+            floatingActionButton.setImageResource(R.drawable.ic_location_disabled_24dp);
+        } else {
+            floatingActionButton.setImageResource(R.drawable.ic_my_location_24dp);
+        }
+        // Enable or disable the location layer on the map
+        map.setMyLocationEnabled(enabled);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_LOCATION: {
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    enableLocation(true);
+                }
+            }
+        }
+    }
+
+    private static class LatLngEvaluator implements TypeEvaluator<LatLng> {
+        // Method is used to interpolate the marker animation.
+
+        private LatLng latLng = new LatLng();
+
+        @Override
+        public LatLng evaluate(float fraction, LatLng startValue, LatLng endValue) {
+            latLng.setLatitude(startValue.getLatitude() +
+                    ((endValue.getLatitude() - startValue.getLatitude()) * fraction));
+            latLng.setLongitude(startValue.getLongitude() +
+                    ((endValue.getLongitude() - startValue.getLongitude()) * fraction));
+            return latLng;
+        }
     }
 }
---
+++ app/src/main/java/work/technie/motonavigator/MotoApplication.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 Anupam Das.
+ */
+
+package work.technie.motonavigator;
+
+import android.app.Application;
+
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+
+/**
+ * Created by anupam on 20/7/16.
+ */
+public class MotoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MapboxAccountManager.start(this, getString(R.string.PUBLIC_TOKEN));
+
+    }
+}
