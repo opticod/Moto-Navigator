2a21fab479be566913ace21d954c762f819f286f
Anupam Das <anupam.das.bwn@gmail.com>
2016-07-20 12:02:14 +0530

    Initial Project Files

---
+++ app/src/main/java/work/technie/motonavigator/MainActivity.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Anupam Das.
+ */
+
+package work.technie.motonavigator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/work/technie/motonavigator/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016 Anupam Das.
+ */
+
+package work.technie.motonavigator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
