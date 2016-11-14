8b10a85250aa192dde11f7b0f9b9b614317ae99b
Anupam Das <anupam.das.bwn@gmail.com>
2016-11-15 01:05:12 +0530

    FEAT: Added AboutFragment

---
+++ app/src/main/java/work/technie/motonavigator/fragment/AboutFragment.java
@@ -0,0 +1,41 @@
+package work.technie.motonavigator.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import work.technie.motonavigator.R;
+
+/**
+ * Created by anupam on 14/11/16.
+ */
+
+public class AboutFragment extends Fragment {
+
+    private final static String TAG = "AboutFragment";
+    private Activity mActivity;
+    private View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = getActivity();
+        rootView = inflater.inflate(R.layout.about, container, false);
+        Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
+        toolbar.setTitle("About");
+        ;
+        DrawerLayout drawer = (DrawerLayout) mActivity.findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                mActivity, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        return rootView;
+    }
+}
